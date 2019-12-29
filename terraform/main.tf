88ce253e2f5a1b849ccc4004729f9b9ce77074c9
root <ycy1766@gmail.com>
2019-12-30 00:15:09 +0900

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,3 +36,14 @@ resource "openstack_compute_instance_v2" "test-server" {
     name = "private"
   }
 }
+
+
+resource "openstack_networking_floatingip_v2" "fip_1" {
+  pool = "public1"
+}
+
+
+resource "openstack_compute_floatingip_associate_v2" "fip_1" {
+  floating_ip = "${openstack_networking_floatingip_v2.fip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.test-server.id}"
+}
