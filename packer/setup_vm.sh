61f9fa4a239fc780f9aa1154fe4f5bf1c541d4a6
root <ycy1766@gmail.com>
2019-12-30 00:30:31 +0900

    test

--- packer/setup_vm.sh
+++ packer/setup_vm.sh
@@ -8,13 +8,13 @@ if [ `id -u` -ne 0 ]; then
     exit 0
 fi
 
-# sudo yum install wget unzip git -y
-# sudo wget https://releases.hashicorp.com/vault-ssh-helper/0.1.4/vault-ssh-helper_0.1.4_linux_amd64.zip
-# sudo unzip vault-ssh-helper_0.1.4_linux_amd64.zip
-# sudo mv vault-ssh-helper /usr/local/bin/
-# sudo rm -rf vault-ssh-helper_0.1.4_linux_amd64.zip
-# sudo cd /opt
-# sudo git clone https://gitlab.com/ycy1766/openstack_devops_2019.git
-# sudo cp -aR openstack_devops_2019/packer/packer_for_sshd/config.hcl /opt
-# sudo cp -aR openstack_devops_2019/packer/packer_for_sshd/sshd /etc/pam.d/sshd
-# sudo cp -aR   openstack_devops_2019/packer/packer_for_sshd/sshd_config  /etc/ssh/sshd_config
+sudo yum install wget unzip git -y
+sudo wget https://releases.hashicorp.com/vault-ssh-helper/0.1.4/vault-ssh-helper_0.1.4_linux_amd64.zip
+sudo unzip vault-ssh-helper_0.1.4_linux_amd64.zip
+sudo mv vault-ssh-helper /usr/local/bin/
+sudo rm -rf vault-ssh-helper_0.1.4_linux_amd64.zip
+sudo cd /opt
+sudo git clone https://gitlab.com/ycy1766/openstack_devops_2019.git
+sudo cp -aR openstack_devops_2019/packer/packer_for_sshd/config.hcl /opt
+sudo cp -aR openstack_devops_2019/packer/packer_for_sshd/sshd /etc/pam.d/sshd
+sudo cp -aR   openstack_devops_2019/packer/packer_for_sshd/sshd_config  /etc/ssh/sshd_config
