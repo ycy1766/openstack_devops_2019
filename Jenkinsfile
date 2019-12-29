13bccdbea031ef951b095bb218a6201766431d86
root <ycy1766@gmail.com>
2019-12-29 23:38:47 +0900

    test

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,3 +11,10 @@ stage('Build_image', {
     sh 'source  /etc/kolla/admin-openrc.sh  && /usr/local/bin/packer  build -color=false packer/packer.json '
   })
 })
+
+
+stage('Deploy_instance', {
+  node('', {
+    sh 'source  /etc/kolla/admin-openrc.sh  && export IMG_ID=`openstack image show packer-2 -c id -f value` && cd  terraform && && terraform apply -var packer_image_name=$IMG_ID '
+  })
+})
---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = "admin"
+  tenant_name = "admin"
+  password    = "adminpassword"
+  auth_url    = "http://10.10.10.100:35357/v3"
+  region      = "RegionOne"
+}
+
+variable "packer_image_name" {
+  description = "packer_image_name"
+}
+
+resource "openstack_blockstorage_volume_v2" "test_vol" {
+  region      = "RegionOne"
+  name        = "cirros_vol"
+  description = "test-cy"
+  image_id    = "${var.packer_image_name}""
+  size        = 3
+}
+
+resource "openstack_compute_instance_v2" "test-server" {
+  name      = "my_instance-2"
+  flavor_id = "1"
+  key_pair  = "mykey"
+  block_device {
+    uuid                  = "${openstack_blockstorage_volume_v2.test_vol.id}"
+    source_type           = "volume"
+    volume_size           = 5
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = true
+  }
+  network {
+    uuid = "b0dd18e6-b0e0-4e62-9805-d7da190aa24d"
+    name = "private"
+  }
+}
